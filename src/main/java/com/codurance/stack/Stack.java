97f76c6fe7d997d7eeca6f47a728e84102e3cd89
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-14 22:09:20 +0200

    stack push ok

--- src/main/java/com/codurance/stack/Stack.java
+++ src/main/java/com/codurance/stack/Stack.java
@@ -1,13 +1,24 @@
 package com.codurance.stack;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Stack {
 
+    private List<Integer> values;
+    protected Stack(){
+        this.values = new ArrayList<Integer>();
+    }
     public static Stack create() {
         return new Stack();
     }
 
     public boolean isEmpty() {
-        return true;
+        return values.size() == 0;
+    }
+
+    public void push() {
+        values.add(0, values.size()+1);
     }
     
 }
--- src/test/java/com/codurance/stack/StackTest.java
+++ src/test/java/com/codurance/stack/StackTest.java
@@ -1,5 +1,6 @@
 package com.codurance.stack;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -10,5 +11,12 @@ public class StackTest {
     public void stack_is_empty(){
         Stack stack = Stack.create();
         assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void stack_push(){
+        Stack stack = Stack.create();
+        stack.push();
+        assertFalse(stack.isEmpty());
     }
 }
